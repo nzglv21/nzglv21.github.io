169af53a3a2a76a1e4d5e1968c7a818073928cb3
niyazgulov <nzg2112@outlook.com>
2024-11-27 10:36:10 +0300

    asdf

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
         tg.expand();
         tg.requestFullscreen();
         tg.disableVerticalSwipes();
+        tg.addToHomeScreen();
     } catch (error) {
         console.log(1);
     }
